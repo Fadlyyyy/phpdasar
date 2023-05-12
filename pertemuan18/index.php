19761eb2ec62ee2672d6b7cbb4e16b0609cef818
Fadlyyyy <fadlyynasah86@f=gmail.com>
2023-05-12 20:32:53 +0700

    done pagination

--- pertemuan18/index.php
+++ pertemuan18/index.php
@@ -186,9 +186,25 @@ if (isset($_POST["cari"])) {
 
 
     <!-- navigasi -->
+
+    <?php if( $halamanAktif > 1 ) : ?>
+        <a href="?halaman=<?= $halamanAktif - 1; ?>">&laquo;</a>
+    <?php endif;  ?>
+
     <?php for( $i = 1; $i <= $jumlahHalaman; $i++ ) : ?>
-        <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
+        <?php if( $i == $halamanAktif ) : ?>
+            <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color:red;"><?= $i; ?></a>
+        <?php else : ?>
+            <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
+        <?php endif; ?>
     <?php endfor; ?>
+
+    <?php if( $halamanAktif < $jumlahHalaman ) : ?>
+        <a href="?halaman=<?= $halamanAktif + 1; ?>">&raquo;</a>
+    <?php endif;  ?>
+
+        <br><br>
+
 
     <br>
         <table border="1" cellpadding="10" cellspacing="0">
